58e4c2d6dddd433027e35cd537b4730026ad55b0
Graham <NA>
2020-05-01 12:01:21 -0700

    Finished search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Search from "./Search";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { storyList: [] };
+    this.boundSubmitSearch = this.submitSearch.bind(this);
+  }
+
+  async submitSearch(term){
+    try {
+      const response = await axios.get(`https://hn.algolia.com/api/v1/search?query=${term}`)
+      const hits = response.data.hits;
+      this.setState({storyList: hits});
+    } catch(err) {
+      console.log("API request error: ", err.message);
+    }
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Search submit={this.boundSubmitSearch} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+    this.boundControlTerm = this.controlTerm.bind(this);
+    this.boundSubmitForm = this.submitForm.bind(this);
+  }
+
+  controlTerm(evt) {
+    this.setState({term: evt.target.value});
+  }
+
+  submitForm(evt) {
+    evt.preventDefault();
+    this.props.submit(this.state.term);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.boundSubmitForm}>
+        <label>Search Hacker News:
+          <input 
+            type="text"
+            value={this.state.term}
+            onChange={this.boundControlTerm}
+          ></input>
+        </label>
+        <button>Search</button>
+      </form>
+    )
+  }
+
+}
+
+export default Search;
